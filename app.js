b9263e4960275396869bb56ddd854ef1e6f238ba
sajid_f57ovgd <sajidhasan2641@gmail.com>
2022-06-24 02:12:12 -0700

    hggh

--- app.js
+++ app.js
@@ -1,51 +1,13 @@
-const sections = document.querySelectorAll(".section");
-const controlsbtn = document.querySelectorAll(".controls") ;
-const controlButton  = document.querySelectorAll(".control");
-const bodyMain = document.querySelector(".main");
-
-
-
-function pageTransition() {
-    // Button active class
-    for(let i = 0; i< controlButton.length; i++){
-        controlButton[i].addEventListener("click",function() {
-            let currentbtn = document.querySelector(".active-btn");
-            if(currentbtn !== null){
-                currentbtn.classList.remove("active-btn");
-            
-            }
-            this.classList += " active-btn";
-            
-           
-        });
-    }
-
-
-    // section active class
-    bodyMain.addEventListener("click", (e)=> {
-        const id = e.target.dataset.id ;
-        if (id){
-            controlsbtn.forEach( function(btn) {
-                btn.classList.remove("active")
-                
-            });
-            e.target.classList.add("active")
-
-            sections.forEach(function(section) {
-                section.classList.remove("active")
-                
-            });
-            const element = document.getElementById(id);
-            element.classList.add("active");
-
-        }
-        
+(function () {
+    [...document.querySelectorAll(".control")].forEach(button => {
+        button.addEventListener("click", function() {
+            document.querySelector(".active-btn").classList.remove("active-btn");
+            this.classList.add("active-btn");
+            document.querySelector(".active").classList.remove("active");
+            document.getElementById(button.dataset.id).classList.add("active");
+        })
     });
-    const themeBtn = document.querySelector(".theme-btn");
-    themeBtn.addEventListener("click",() =>{
+    document.querySelector(".theme-btn").addEventListener("click", () => {
         document.body.classList.toggle("light-mode");
-
     })
-}
-
- pageTransition();
+})();
